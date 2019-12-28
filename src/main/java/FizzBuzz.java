1058c88027af315d6d6e2d4ba6849e842d3b38cc
easylliu <15010250376@163.com>
2019-12-28 14:01:28 +0800

    refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,17 +1,12 @@
 import static java.lang.String.format;
 
 public class FizzBuzz {
-    public static void main(String [] args) {
-        for (int i = 1 ; i <= 100 ; i++) {
-            String printNum = transform(i);
-            System.out.println(printNum);
-        }
-    }
     public static String transform(int number) {
         String returnStr = "";
 //        if(number < 1) {
 //            throw new IllegalArgumentException("invalid input");
 //        }
+//
         if(number % 3 == 0) {
             returnStr += "Fizz";
         }
@@ -21,7 +16,7 @@ public class FizzBuzz {
         if(number % 7 == 0) {
             returnStr += "Whizz";
         }
-        if(returnStr .equals("")) {
+        if(returnStr.equals("")) {
             return format("%d", number);
         }
         return returnStr;
