35a5d958027da193cc14ea8b995052e9c9fa593e
easylliu <15010250376@163.com>
2019-12-28 13:44:34 +0800

    refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,7 @@
 import static java.lang.String.format;
 
 public class FizzBuzz {
-    public static void main(String [] args){
+    public static void main(String [] args) {
         for (int i = 1 ; i <= 100 ; i++) {
             String printNum = transform(i);
             System.out.println(printNum);
